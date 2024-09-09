6fa955ec509fcc93b4cd38aa63c3b16c0dc5c21c
urzp <ermak80_pass@mail.ru>
2024-09-09 13:18:07 +0300

    submenu

--- src/components/PageParts/Header.vue
+++ src/components/PageParts/Header.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="header-wrap">
-        <div class="logo">
-            <img src="@/assets/imgs/logo.svg" alt="">
+    <div class="header">
+        <div class="header-row">
+            <div class="logo"><img src="@/assets/imgs/logo.svg" alt=""></div>
+            <div class="menu">
+                <div class="item_menu">About project</div>
+                <div class="item_menu">How to use</div>
+            </div>
+            <div class="btn_menu"></div>
         </div>
-        <div class="menu">
-            <div class="item_menu">About project</div>
-            <div class="item_menu">How to use</div>
-        </div>
-        <div class="btn_menu">
+        <div class="sub-header">
+            <div class="item_subHeader">New Project</div>
+            <div class="item_subHeader">Open Project</div>
         </div>
     </div>
 </template>
@@ -21,7 +24,7 @@ export default{
 
 
 <style scoped>
-    .header-wrap{
+    .header-row{
         height: 110px;
         display: flex;
         align-items: center;
@@ -34,6 +37,9 @@ export default{
         align-items: center;
         justify-content: center;
         column-gap: 100px;
+    }
+    .logo{
+        cursor: pointer;
     }
     .menu .item_menu{
         font-family: 'Raleway-ExtraLight';
@@ -58,6 +64,16 @@ export default{
     .btn_menu:hover{
         background-image: url('@/assets/icons/btn_menu/active.svg');
         transition: var(--transition-hover);
+    }
+
+    .sub-header{
+        margin-top: 15px;
+        display: flex;
+        column-gap: 50px;
+        font-size: 'Raleway-ExtraLight';
+        font-size: 16px;
+        color: #999999;
+        cursor:pointer;
     }
 
 </style>
