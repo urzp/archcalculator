3aba5eea0384a9cc9f46395f300bae5d72abcd63
urzp <ermak80_pass@mail.ru>
2024-09-18 09:18:55 +0300

    input price

--- src/components/UI/Price.vue
+++ src/components/UI/Price.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="price">
-        <template v-if="true">
+        <template v-if="!input_type">
         <div class="unit-price">{{ price.unit }}</div>
         <div class="comma">,</div>
         <div class="cents">{{ price.cent }}</div>
         </template>
         <template v-if="input_type">
-            <input ref="price_input"  type="text" class="unit-price" :value="price.unit" @change="event => edit_price( event.target.value, price.cent ) " @input="event => validate(event.target.value)"/>
+            <input  type="text" class="unit-price" :value="price.unit" @change="event => edit_price( event.target.value, price.cent )" @input="event => { validate(event) }"/>
             <div class="comma">,</div>
-            <input  type="number" max="99" min="0" class="cents" :value="price.cent"  @change="event => edit_price( price.unit, event.target.value ) "/>      
+            <input  type="number" max="99" min="0" class="cents" :value="price.cent"  @change="event => edit_price( price.unit, event.target.value )"  @input="event => { validate(event, true) }"/>      
         </template>
         <div class="currency-sign">€</div>
     </div>
@@ -17,6 +17,14 @@
 <script>
 export default{
     name: 'Price',
+    data(){
+        return{
+            price:{
+                unit:'0',
+                cent:'00'
+            }
+        }
+    },
     props:{
         value:[String, Number],
         input_type:{ 
@@ -35,39 +43,51 @@ export default{
             type:String,
             default: 'Comfortaa-Regular',         
         },
+    },    
+    watch: {
+        value(){
+            this.count_price()
+        }
     },
     emits:['edit_price'],
     methods:{
-        validate(val){
-            if(/[^0-9.]/g.test(val)) this.$refs.price_input.value = val.toString().replace(/[^0-9]/g, '');
+        validate(event, cent_section=false){
+            let val = event.target.value
+            let position = event.target.selectionStart - 1
+            if(/[^0-9]/g.test(val)){
+                event.target.value = val.toString().replace(/[^0-9]/g, '');
+                event.target.setSelectionRange(position, position)
+            }
+            if(cent_section){
+                let cent = event.target.value
+                if( cent.length > 2 ) {
+                    event.target.value = cent.slice(0,-1)
+                }
+            }
         },
         edit_price(unit, cent){
-            console.log('input = ', unit, cent)
             unit = unit.toString().replace(/[^0-9]/g, '');
             cent = cent.toString().replace(/[^0-9]/g, '');
-            if( cent.length > 2 ) cent = cent.slice(cent.length - 2)
-            console.log(unit, cent)
+            if( cent.length > 2 ) cent = cent.slice(0,-1)
             let newPrice = Number(unit) + 0.01 * Number(cent)
-            console.log(newPrice)
             this.$emit('edit_price', newPrice)
+            this.count_price()
         },
-    },
-    computed: {
-        price(){
+        count_price(){
             let val = Number(this.value)
             val = val?val:0
             val = Math.round(val * 100) / 100 
             let cent  = String(val).split('.')[1]
             cent = cent?cent:'00'
-            cent = cent.length == 1?cent+'0':cent
+            cent = cent.length == 1?`${cent}0`:cent
             let unit = Math.floor(val)
             unit = unit.toLocaleString("de-DE")
-            return {
+            this.price = {
                 unit,
                 cent
             }
         }
-    },
+    }
 }
 </script>
 
@@ -82,14 +102,12 @@ export default{
     .unit-price{
         text-align: right;
         font-size: 18px;
-        color:#838383;
     }
     .cents{
         font-size: v-bind(font_size_cent);
         text-align: right;
         width: 21px;
         font-size: 16px;
-        color:#838383;
     }
     .currency-sign{
         margin-left: 15px;
