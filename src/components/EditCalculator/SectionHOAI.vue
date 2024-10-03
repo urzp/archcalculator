58e5a7b963d6d6ca050f13b9e6cd3f52e29c257d
urzp <ermak80_pass@mail.ru>
2024-10-03 23:12:45 +0300

    save

--- src/components/EditCalculator/SectionHOAI.vue
+++ src/components/EditCalculator/SectionHOAI.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="wrap">
-        <div class="title">Paragraph:</div>
-        <div class="active-paragraph" >{{ data.name }}</div>
+        <div class="label">Paragraph:</div>
+        <div class="paragraph-name" >{{ data.name }}</div>
+        <div class="paragraph-title" >{{ data.title }}</div>
+        <Select_List  right :data='data' />
     </div>
 </template>
 
@@ -11,8 +13,12 @@ export default{
     data(){
         return{
             data:{
+                id:'',
                 name:'§39',
                 title:'Freianlagen',
+                list:{
+                    
+                }
             }
         }
     }
@@ -22,10 +28,15 @@ export default{
 <style scoped>
     .wrap{
         margin-top: 30px;
-    }
-
-    .title{
+        min-height: 75px;
+        display: flex;
+        align-items: center;
+        column-gap: 15px;
         font-family: 'Raleway-Light';
         font-size: 20px;
+    }
+
+    .paragraph-name, .paragraph-title{
+        font-family: 'Raleway-Medium';
     }
 </style>
