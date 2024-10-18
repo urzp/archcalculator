dba5733800255083523eb3f5c464cabbb06a931a
urzp <ermak80_pass@mail.ru>
2024-10-18 15:50:30 +0300

    save

--- public/php/dataServis/get/index.php
+++ public/php/dataServis/get/index.php
@@ -1,30 +1,14 @@
 <?php
 
 switch ($typeData) {
-    case 'allowableCosts':
-        include 'allowableCosts.php';
-        break;
-    case 'SubStage':
-        include 'SubStage.php';
-        break;
-    case 'Stages':
-        include 'Stages.php';
-        break;
-    case 'HonorarZone':
-        include 'HonorarZone.php';
-        break;
-    case 'RequirementsPoints':
-        include 'RequirementsPoints.php';
-        break;
-    case 'FeeTable':
-        include 'FeeTable.php';
-        break;
-    case 'Paragraphs':
-        include 'Paragraphs.php';
-        break;
-    case 'getHOAI':
-        include 'HOAI.php';
-        break;
+    case 'allowableCosts': include 'read/allowableCosts.php'; break;
+    case 'SubStage': include 'read/SubStage.php'; break;
+    case 'Stages': include 'read/Stages.php'; break;
+    case 'HonorarZone': include 'read/HonorarZone.php'; break;
+    case 'RequirementsPoints': include 'read/RequirementsPoints.php'; break;
+    case 'FeeTable': include 'read/FeeTable.php'; break;
+    case 'Paragraphs': include 'read/Paragraphs.php'; break;
+    case 'getHOAI': include 'read/HOAI.php'; break;
 }
 
 ?>
