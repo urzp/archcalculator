f13834bc9d4ad7801eba66d7bb48240d57a9bf2c
urzp <ermak80_pass@mail.ru>
2024-11-01 23:18:08 +0300

    save

--- public/php/dataServis/get/read_calc/requirementsPoints.php
+++ public/php/dataServis/get/read_calc/requirementsPoints.php
@@ -3,6 +3,10 @@ $id = $rq_data -> id;
 $selector = "`id_paragraph` = '$id' ORDER BY `id` ASC, cast(`number` as unsigned) ASC";
 $data = crud_read('requirementsPoints',"*", $selector);
 
+foreach($data as $key=>$item){
+    $data[$key]['value']= $item['minPoint'];
+}
+
 $result = (object) [
     'success' => true,
     'data' => $data,
