fa3392238f2f26cb7d17e4527813c84826461ae5
urzp <ermak80_pass@mail.ru>
2024-12-02 16:03:11 +0300

    save

--- src/components/Calculator/_Object/01_Basis/Detals_of_item/02_FinanceDetal.vue
+++ src/components/Calculator/_Object/01_Basis/Detals_of_item/02_FinanceDetal.vue
@@ -7,10 +7,60 @@
                     <Price input_type font_size_unit="18px" :value="item.value" @edit_price="val=>updatePrice(item.id, val)" :typeCurrancy="typeCurrancy"/>
                 </div>
             </div>
+            <div v-if="has_400_Bauwerk" class="detal-item">
+                <div class="title">100-800 Summe</div>
+                <div class="value-wrap">
+                    <Price font_size_unit="18px" :value="summ_100_800" />
+                </div>
+            </div>
+            <div v-if="has_400_Bauwerk"  class="kg400_Bauwerk">
+                <div class="row header">
+                    <div class="name_item"></div>
+                    <div class="imput_calc">Hilfswert</div>
+                    <div class="percent"></div>
+                    <div class="output_calc">Anrechenbar</div>
+                    <div class="total_calc"></div>
+                </div>
+                <div class="row">
+                    <div class="name_item">hiervon sonstige anrechenbare Kosten</div>
+                    <div class="imput_calc"></div>
+                    <div class="percent"></div>
+                    <div class="output_calc"><Price font_size_unit="16px" :value="summ_other"/></div>
+                    <div class="total_calc"></div>             
+                </div>
+                <div class="row">
+                    <div class="name_item">hiervon bedingt anrechenbare Kosten</div>
+                    <div class="imput_calc"><Price font_size_unit="16px" :value="kg400"/></div>
+                    <div class="percent"></div>
+                    <div class="output_calc"></div>
+                    <div class="total_calc"></div>             
+                </div>
+                <div class="row">
+                    <div class="name_item left_gap">{{ `hiervon zu 100% anrechenbar (25% von ${summ_other}€)` }}</div>
+                    <div class="imput_calc"><Price font_size_unit="16px" :value="until_25_per"/></div>
+                    <div class="percent">100%</div>
+                    <div class="output_calc"><Price font_size_unit="16px" :value="until_25_per"/></div>
+                    <div class="total_calc"></div>             
+                </div>
+                <div class="row">
+                    <div class="name_item left_gap">{{ `hiervon zu 50% anrechenbar (über 25% von ${summ_other}€)` }}</div>
+                    <div class="imput_calc"><Price font_size_unit="16px" :value="after_25_per"/></div>
+                    <div class="percent">50%</div>
+                    <div class="output_calc"><Price font_size_unit="16px" :value="after_25_per*0.5"/></div>
+                    <div class="total_calc"></div>             
+                </div>
+                <div class="row summ_row">
+                    <div class="name_item">Summe nicht anrechenbare Kosten</div>
+                    <div class="imput_calc"></div>
+                    <div class="percent"></div>
+                    <div class="output_calc"></div>
+                    <div class="total_calc"><Price font_size_unit="16px" :value="(-1)*after_25_per*0.5"/></div>             
+                </div>
+            </div>
             <div class="total">
                 <div class="total_points">
                     <div class="label">Total eligible costs, "other": </div>
-                    <div class="value"> <Price  font_size_unit="18px" :value="total"  :typeCurrancy="typeCurrancy"/> </div>
+                    <div class="value"> <Price  font_size_unit="16px" :value="total"  :typeCurrancy="typeCurrancy"/> </div>
                 </div>
             </div>
         </div>
@@ -18,7 +68,9 @@
 </template>
 
 <script>
+import Price from '@/components/UI/Simples/Price.vue'
 export  default{
+  components: { Price },
     name: 'FinanceDetal',
     data(){
         return{
@@ -27,9 +79,37 @@ export  default{
     },
     emits:['useDetals', 'edit_UserTitle' ,'edit_price'],
     computed:{
-        total(){
-            let result = this.list.reduce((sum, item) => sum + Number(item.value),0)
+        has_400_Bauwerk(){
+            let result = !!this.list.find(item=>item.name.includes('400'))
             return result
+        },
+        kg400(){
+            if(!this.has_400_Bauwerk) return 0
+            let kg400 =  this.list.find(item=>item.name.includes('400')).value
+            return Number( kg400 )
+        },
+        until_25_per(){
+            if(!this.has_400_Bauwerk) return 0
+            let result = this.summ_other*0.25
+            if(this.kg400 < result) return this.kg400
+            return this.summ_other*0.25
+        },
+        after_25_per(){
+            if(!this.has_400_Bauwerk) return 0
+            return this.kg400 - this.until_25_per
+        },
+        summ_other(){
+            if(!this.has_400_Bauwerk) return 0
+            return this.summ_100_800 - this.kg400
+        },
+        summ_100_800(){
+            if(!this.has_400_Bauwerk) return 0
+            return Number( this.list.reduce((sum, item) => sum + Number(!item.value?0:item.value),0) )
+        },
+        total(){
+            let result = Number( this.list.reduce((sum, item) => sum + Number(!item.value?0:item.value),0) )
+            result = result - this.after_25_per*0.5
+            return Number(result)
         }
     },
     methods:{
@@ -115,7 +195,61 @@ export  default{
     font-size: 18px;
 }
 
+.kg400_Bauwerk{
+    margin-left:40px;
+    color: #8E8E8E;
+    font-family: 'Raleway-Light';
+    font-size: 16px;
+}
 
+.kg400_Bauwerk .row{
+    display: flex;
+}
+
+.header{
+    border-bottom: 1px solid #c5c5c5;
+    margin-bottom: 10px;  
+    padding-bottom: 6px;
+}
+
+.name_item{
+    width: 50%
+}
+
+.name_item.left_gap{
+    padding-left: 40px;
+}
+
+.imput_calc{
+    width: 12.5%;
+    display: flex;
+    justify-content: flex-end;
+}
+.percent{
+    width: 12.5%;
+    text-align: center;
+}
+.output_calc{
+    width: 12.5%;
+    display: flex;
+    justify-content: flex-end;
+}
+.total_calc{
+    width: 12.5%;
+    display: flex;
+    justify-content: flex-end;
+}
+
+.summ_row{
+    border-top: 1px solid #bdbdbd;
+    padding-top: 10px;
+    margin-top: 6px;
+
+}
+
+.kg400_Bauwerk .header > div{
+    justify-content: center;
+}
 
 .total_points{
     display: flex;
