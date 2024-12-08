df154b2ec26ac6fae441c578630282c6fc23b971
urzp <ermak80_pass@mail.ru>
2024-12-08 22:01:11 +0300

    save

--- public/php/dataServis/04_users/avatar.php
+++ public/php/dataServis/04_users/avatar.php
@@ -5,28 +5,40 @@ $token = $_POST['token'];
 
 $target_dir_ = $_SERVER['DOCUMENT_ROOT'].'/users/user_'.$user_id.'/avatar';
 
+$allowed = array('gif', 'png', 'jpg', 'svg');
+$filename = $_FILES['avatar']['name'];
+$ext = pathinfo($filename, PATHINFO_EXTENSION);
 
-removeDirectory($target_dir_);
-if (!file_exists($target_dir_)) {
-    mkdir($target_dir_, 0777, true);
-}
+if (!in_array($ext, $allowed)) {
 
-if(basename( $_FILES['avatar']['name'])!=''){
-    $new_name_file = time().'_'.basename( $_FILES['avatar']['name']);
-    $target_file = $target_dir_.'/'.$new_name_file;
-    move_uploaded_file($_FILES['avatar']['tmp_name'], $target_file);
+    $result = (object) [
+        'success' => false,
+    ];
+
 }else{
-    $new_name_file = '';
+
+    removeDirectory($target_dir_);
+    if (!file_exists($target_dir_)) {
+        mkdir($target_dir_, 0777, true);
+    }
+
+    if(basename( $_FILES['avatar']['name'])!=''){
+        $new_name_file = time().'_'.basename( $_FILES['avatar']['name']);
+        $target_file = $target_dir_.'/'.$new_name_file;
+        move_uploaded_file($_FILES['avatar']['tmp_name'], $target_file);
+    }else{
+        $new_name_file = '';
+    }
+
+    $data['avatar'] = $new_name_file;
+    $selector = " `id` = '$user_id' AND `token` = '$token' ";
+    crud_update('users', $data, $selector);
+
+    $result = (object) [
+        'success' => true,
+    ];
+
 }
-
-$data['avatar'] = $new_name_file;
-$selector = " `id` = '$user_id' AND `token` = '$token' ";
-crud_update('users', $data, $selector);
-
-$result = (object) [
-    'success' => true,
-];
-
 
 function removeDirectory($dir) {
     if ($objs = glob($dir . '/*')) {
