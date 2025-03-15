a4c8519952c0e03f2a053700a585b4265f6b8145
urzp <ermak80_pass@mail.ru>
2025-03-15 17:34:06 +0300

    save

--- public/php/dataServis/05_exportDoc/XFactor/xml_data/05_ApplicableHeaderTradeSettlement/index.php
+++ public/php/dataServis/05_exportDoc/XFactor/xml_data/05_ApplicableHeaderTradeSettlement/index.php
@@ -44,7 +44,7 @@ $xml->startElementNS('ram', 'ApplicableHeaderTradeSettlement', null);
 foreach($bill -> previos_payments as $item){
 
     if($item -> paid_value == '0'){
-        $ChargeIndicator = 'true';
+        $ChargeIndicator = 'false';
         $Reason = 'Zahlung vom -';
         $ActualAmount = '0';
     }else{
@@ -68,36 +68,89 @@ foreach($bill -> previos_payments as $item){
 
 }
 
+    $tax = (float)($bill -> tax);
+    $CalculatedAmount = $bill -> payment_total * ($tax/100);
+    $CalculatedAmount = number_format($CalculatedAmount, 2, '.', '' );
+    $tax = number_format($tax, 1, '.', '' );
+    $BasisAmount = number_format($bill -> payment_total, 2, '.', '' );
+
     $xml->startElementNS('ram', 'ApplicableTradeTax', null);
         $xml->startElementNS('ram', 'CalculatedAmount', null);
-            $xml->text( $ActualAmount );
+            $xml->text( $CalculatedAmount );
         $xml->endElement();
         $xml->startElementNS('ram', 'TypeCode', null);
             $xml->text( 'VAT' );
         $xml->endElement();
         $xml->startElementNS('ram', 'BasisAmount', null);
-            $xml->text( '132734.19' );
+            $xml->text( $BasisAmount );
         $xml->endElement();
         $xml->startElementNS('ram', 'CategoryCode', null);
             $xml->text( 'S' );
         $xml->endElement();
         $xml->startElementNS('ram', 'RateApplicablePercent', null);
-            $xml->text( '19' );
+            $xml->text( $tax );
         $xml->endElement();
     $xml->endElement();
 
+    $payment_date = (new DateTime($bill -> payment_date -> bis ))->format('d.m.Y') ;
+
     $xml->startElementNS('ram', 'SpecifiedTradePaymentTerms', null);
         $xml->startElementNS('ram', 'Description', null);
-            $xml->text( 'Bitte zahlen Sie den Betrag bis zum 5.3.2025 an die unten genannte Bankverbindung.' );
+            $xml->text( "Bitte zahlen Sie den Betrag bis zum $payment_date an die unten genannte Bankverbindung." );
         $xml->endElement();
         $xml->startElementNS('ram', 'DueDateDateTime', null);   
             $xml->startElementNS('udt', 'DateTimeString', null);   
                 $xml->writeAttribute('format', '102');
-                $xml->text( '20250305' );
+                $xml->text( (new DateTime($bill -> payment_date -> bis ))->format('Ymd') );
             $xml->endElement();
         $xml->endElement();
     $xml->endElement();
 
+
+    $LineTotalAmount =$bill->total_net;
+    $LineTotalAmount = number_format($LineTotalAmount, 2, '.', '' );
+
+    $TaxBasisTotalAmount = $LineTotalAmount;
+
+    $TaxTotalAmount = $bill->total_tax;
+    $TaxTotalAmount = number_format($TaxTotalAmount, 2, '.', '' );
+
+    $GrandTotalAmount = $bill-> total;
+    $GrandTotalAmount = number_format($GrandTotalAmount, 2, '.', '' );
+
+    $TotalPrepaidAmount = $bill-> previos_payments_total;
+    $TotalPrepaidAmount = number_format($TotalPrepaidAmount, 2, '.', '' );
+
+    $DuePayableAmount = $bill-> payment_total;
+    $DuePayableAmount = number_format($DuePayableAmount, 2, '.', '' );
+
+    $xml->startElementNS('ram', 'SpecifiedTradeSettlementHeaderMonetarySummation', null);
+        $xml->startElementNS('ram', 'LineTotalAmount', null);
+            $xml->text( $LineTotalAmount  );
+        $xml->endElement();
+        $xml->startElementNS('ram', 'ChargeTotalAmount', null); //not used
+            $xml->text( '0' );
+        $xml->endElement();
+        $xml->startElementNS('ram', 'AllowanceTotalAmount', null); //not used
+            $xml->text( '0' );
+        $xml->endElement();
+        $xml->startElementNS('ram', 'TaxBasisTotalAmount', null);
+            $xml->text( $TaxBasisTotalAmount  );
+        $xml->endElement();
+        $xml->startElementNS('ram', 'TaxTotalAmount', null);
+            $xml->writeAttribute('currencyID', 'EUR');
+            $xml->text( $TaxTotalAmount  );
+        $xml->endElement();
+        $xml->startElementNS('ram', 'GrandTotalAmount', null);
+            $xml->text( $GrandTotalAmount  );
+        $xml->endElement();
+        $xml->startElementNS('ram', 'TotalPrepaidAmount', null);
+            $xml->text( $TotalPrepaidAmount  );
+        $xml->endElement();
+        $xml->startElementNS('ram', 'DuePayableAmount', null);
+            $xml->text( $DuePayableAmount  );
+        $xml->endElement();
+    $xml->endElement();
 
 $xml->endElement(); 
 
