61600f4d08bb8cb961d8e8fa590762f804798e2a
urzp <ermak80_pass@mail.ru>
2025-03-21 11:41:31 +0300

    save

--- public/php/dataServis/05_exportDoc/pdf/bill/pdf.php
+++ public/php/dataServis/05_exportDoc/pdf/bill/pdf.php
@@ -42,6 +42,8 @@ $project_summe = toMoney($project->total);
 $invoice_number = $project->invoice_number;
 $greeting_phrase = $project->greeting_phrase;
 
+$previos_payments_total = toMoney($project->previos_payments_total);
+$rest_payment_total = toMoney($project->payment_total);
 
 require_once ($_SERVER['DOCUMENT_ROOT'].'/php/lib/dompdf/autoload.inc.php');
 use Dompdf\Dompdf;
@@ -133,13 +135,88 @@ $html.="   <div class='title_2'>Zusammenfassung</div>
                         <td class='title_3 number' style='width: 25%; text-align: right;'>$project_summe</td>
                     </tr>
                 </table>
+                <div class='title_3 number' style='width: 640px; text-align: right;'>Summe Zusammenfassung:  $project_summe</div>
+
+                <div class='title_2'>Gesamt- und Resthonoraranspruch</div>
+
+                <table class='row_style' style='width: 680px;'> 
+                    <tr>
+                        <td class='title_3' style='width: 50%; padding-left: 20px; text-align: left;'>Rechnerisches Gesamthonorar</td>
+                        <td class='title_3' style='width: 25%; text-align: left;'></td>
+                        <td class='title_3 number' style='width: 25%; text-align: right; '>$project_summe</td>
+                    </tr>
+                    <tr>
+                        <td class='title_3' style='width: 75%; padding-left: 20px; text-align: left;'>Abzüge für geleistete Abschlagszahlungen</td>
+                        <td class='title_3' style='width: 0%; text-align: left;'></td>
+                        <td class='title_3 number' style='width: 25%; text-align: right; '>-$previos_payments_total</td>
+                    </tr>
+                </table>";
+
+$html.=          "<table style='width: 680px; line-height: 0.8; margin-top:-20px;'>";
+foreach($project->previos_payments as $item){
+    $paid_date = date('d.m.Y',strtotime($item->paid_date));
+    $paid_value = toMoney($item->paid_value);
+    $html.="
+                
+                    <tr>
+                        <td class='title_4' style='width: 20%; padding-left: 20px; text-align: left;'>$item->invoice_number</td>
+                        <td class='title_4' style='width: 20%; text-align: center;'>Zalung vom</td>
+                        <td class='title_4' style='width: 20%; text-align: center;'>$paid_date</td>
+                        <td class='title_4 number' style='width: 20%; text-align: center; '>$paid_value</td>
+                        <td class='title_4 number' style='width: 20%; text-align: right; '></td>
+                    </tr>     
+    ";
+};
+$html.=             "</table>";
+
+$html.= "       
+                <table class='row_style' style='width: 680px;'>     
+                <tr>
+                    <td class='title_3' style='width: 50%; padding-left: 20px; text-align: left;'>Restlicher Honoraranspruch</td>
+                    <td class='title_3' style='width: 25%; text-align: left;'></td>
+                    <td class='title_3 number' style='width: 25%; text-align: right; '>$rest_payment_total</td>
+                </tr>
+                </table>
+
                 <div class='total_project'>
                     <div class='title_1'>$project_name</div>
-                    <div class='title_1 number-bold total_project_Summe'>Summe: $project_summe </div>
+                    <div class='title_1 number-bold total_project_Summe'>Summe: $rest_payment_total </div>
                 </div>
             </div>
+            
         </div>
 ";
+
+$tax_rest_total = toMoney($project->payment_total * ((float)$project->tax)/100);
+
+$html.= "<div class='title_2 project_description'>Im Betrag von $rest_payment_total sind $tax_rest_total Mehrwertsteuer enthalten. Bitte zahlen Sie den Betrag bis zum $payment_date_bis an die unten genannte Bankverbindung.</div>";
+
+$html.="
+        <table style='width: 680px; line-height: 0.8; margin-top:10px;'>
+            <tr>
+                <td class='title_3' style='width: 25%; padding-left: 20px; text-align: left;'>Kontoinhaber</td>
+                <td class='title_3' style='width: 25%; text-align: left;'></td>
+            </tr>
+            <tr>
+                <td class='title_3' style='width: 25%; padding-left: 20px; text-align: left;'>IBAN:</td>
+                <td class='title_3' style='width: 25%; text-align: left;'>$project->user_IBAN</td>
+            </tr>
+            <tr>
+                <td class='title_3' style='width: 25%; padding-left: 20px; text-align: left;'>BIC:</td>
+                <td class='title_3' style='width: 25%; text-align: left;'>$project->user_BIC</td>
+            </tr>
+            <tr>
+                <td class='title_3' style='width: 25%; padding-left: 20px; text-align: left;'>Institut:</td>
+                <td class='title_3' style='width: 25%; text-align: left;'>$project->user_Institut</td>
+            </tr>
+            <tr>
+                <td class='title_3' style='width: 25%; padding-left: 20px; text-align: left;'>USt-Id.-Nr.</td>
+                <td class='title_3' style='width: 25%; text-align: left;'>$project->user_USt</td>
+            </tr>
+        </table>
+";
+
+$html .="<div class='title_2 project_description'>Gem. § 14b Abs. 1 S.4 Umsatzsteuergesetz muss diese Rechnung und ein Zahlungsbeleg für zwei Jahre aufbewahrt werden. Die Aufbewahrungsfrist beginnt mit dem Ende des Kalenderjahres, in dem die Rechnung ausgestellt wurde. Ein Verstoß kann nach § 26a Abs. 2 UStG mit bis zu 500 EUR Geldbuße geahndet werden.</div>";
         
 $html .="
     </body>
