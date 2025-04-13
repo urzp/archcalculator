67cff603a7821c6fb0ffb88053b140b649e567a7
urzp <ermak80_pass@mail.ru>
2025-04-13 11:14:49 +0300

    save

--- public/php/dataServis/06_admin/AdminUserProject.php
+++ public/php/dataServis/06_admin/AdminUserProject.php
@@ -10,7 +10,9 @@ $projects = crud_read('projects', $collums, $selector);
 foreach($projects as $key=>$item){
     $id_project = $item['id'];
     $selector="`project_id`='$id_project'";
-    $projects[$key]['bills'] = crud_count('project_bills', $selector);
+    $projects[$key]['bills_count'] = crud_count('project_bills', $selector);
+    $collums='id, created, name, paid_date, locked, downLoad_token';
+    $projects[$key]['bills'] = crud_read('project_bills', $collums, $selector);
 }
 
 
