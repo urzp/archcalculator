0e8ae5e271da647cad47dff06e50623ab3bf758c
urzp <ermak80_pass@mail.ru>
2025-06-18 20:06:27 +0300

    stripe

---
+++ public/php/dataServis/stripe.php
@@ -0,0 +1,62 @@
+<?php
+
+include $_SERVER['DOCUMENT_ROOT'].'/php/logs/log.php';
+//push_log(json_encode($_POST), basename(__FILE__), 'stipe_log');
+push_log('test', basename(__FILE__), 'stipe_log');
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/php/lib/stripe-php-master/init.php';
+
+$stripeSecretKey = 'sk_test_51RahmuPtu9HDKCGxVqgDG1i8Wquf4q2bdcSLWxUr8jM1wdrylAq7VJWFPg9CgUHaToladMxr29iVQZmbIzup2BeW00Hz7tZG1x';
+
+\Stripe\Stripe::setApiKey($stripeSecretKey);
+$endpoint_secret = 'whsec_nSlykMG31kD7YwvOd8tC0asdXrIoxJBE';
+
+$payload = @file_get_contents('php://input');
+$event = null;
+
+try {
+  $event = \Stripe\Event::constructFrom(
+    json_decode($payload, true)
+  );
+} catch(\UnexpectedValueException $e) {
+  // Invalid payload
+  echo '⚠️  Webhook error while parsing basic request.';
+  http_response_code(400);
+  exit();
+}
+if ($endpoint_secret) {
+  // Only verify the event if there is an endpoint secret defined
+  // Otherwise use the basic decoded event
+  $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+  try {
+    $event = \Stripe\Webhook::constructEvent(
+      $payload, $sig_header, $endpoint_secret
+    );
+  } catch(\Stripe\Exception\SignatureVerificationException $e) {
+    // Invalid signature
+    echo '⚠️  Webhook error while validating signature.';
+    http_response_code(400);
+    exit();
+  }
+}
+
+// Handle the event
+switch ($event->type) {
+  case 'payment_intent.succeeded':
+    $paymentIntent = $event->data->object; // contains a \Stripe\PaymentIntent
+    // Then define and call a method to handle the successful payment intent.
+    // handlePaymentIntentSucceeded($paymentIntent);
+    break;
+  case 'payment_method.attached':
+    $paymentMethod = $event->data->object; // contains a \Stripe\PaymentMethod
+    // Then define and call a method to handle the successful attachment of a PaymentMethod.
+    // handlePaymentMethodAttached($paymentMethod);
+    break;
+  default:
+    // Unexpected event type
+    error_log('Received unknown event type');
+}
+
+http_response_code(200);
+
+?>
