56e065f5f3b1747dc6c789d617c7bb4ea021fca4
urzp <ermak80_pass@mail.ru>
2025-08-21 13:16:03 +0300

    save

--- public/php/tests/test_01.php
+++ public/php/tests/test_01.php
@@ -1,13 +1,5 @@
 <?php
 //https://honorar.online/php/tests/test_01.php
-// include $_SERVER['DOCUMENT_ROOT'].'/php/config.php'; $mysql = $mysql_calc;
-// include $_SERVER['DOCUMENT_ROOT'].'/php/modules/crud.php';
-// require_once $_SERVER['DOCUMENT_ROOT'].'/php/logs/log.php';
 
-// $setApiKey = crud_read('settings', 'value', "`name`='setApiKey'")[0]["value"];
-// $signingSecret = crud_read('settings', 'value', "`name`='signingSecret'")[0]["value"];
-
-// echo 'begin test_01'.'<br>';
-// echo var_dump($signingSecret);
 
 ?>
