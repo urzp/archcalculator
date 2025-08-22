6148e8cca2e89064e90de4ce417c95f6a2505fb4
urzp <ermak80_pass@mail.ru>
2025-08-22 13:48:58 +0300

    save

--- public/php/dataServis/08_stripe/04_subscription_updated.php
+++ public/php/dataServis/08_stripe/04_subscription_updated.php
@@ -1,26 +1,36 @@
 <?php
-    push_log('invoice.payment_succeeded', basename(__FILE__), 'stipe_log');  
-    $data_object = $event->data->object;
-    $stripe_id = $data_object->id;
-    $stripe_custemer_id = $data_object->customer;
-    $currentPeriodEndDate = $data_object->period_end;
-    $currentPeriodEndDate = date('Y-m-d H:i:s', $currentPeriodEndDate);
+push_log('customer.subscription.updated', basename(__FILE__), 'stipe_log'); 
+push_log('$event: '.$event, basename(__FILE__), 'stipe_log');
+$data_object = $event->data->object;
+$stripe_custemer_id = $data_object->customer;
+$subscription_id = $data_object->id;
+push_log('$data_object: '.$data_object, basename(__FILE__), 'stipe_log');
+push_log('$stripe_custemer_id: '.$stripe_custemer_id, basename(__FILE__), 'stipe_log');
+push_log('$subscription_id: '.$subscription_id, basename(__FILE__), 'stipe_log');
 
-    $mysql = $mysql_calc;
+$subscription = $data_object;
+$lastItemIndex = count($subscription['items']['data']) - 1; 
+$currentPeriodEnd = $subscription['items']['data'][$lastItemIndex]['current_period_end'];
+$currentPeriodEndDate = date('Y-m-d H:i:s', $currentPeriodEnd);
 
-    $selector = "`stripe_custemer_id`='$stripe_custemer_id'";
-    $findUser = crud_read('users', 'id' ,$selector)[0]['id'];
-    if($findUser==''){
-        echo  'null';
-        $data_err['event']="invoice.payment_succeeded";
-        $data_err['id_event']=$stripe_id;
-        $data_err['comment']="Not find user with reference stripe_custemer_id $stripe_custemer_id";
-        crud_create('errors_stripe', $data_err);
-    }else{
-        $update_data['subscription_period_end']=$currentPeriodEndDate;
-        crud_update('users', $update_data, $selector);
-    }
+$mysql = $mysql_calc;
 
-    push_log('stripe_custemer_id: '.$stripe_custemer_id, basename(__FILE__), 'stipe_log');
-    push_log('currentPeriodEndDate: '.$currentPeriodEndDate, basename(__FILE__), 'stipe_log');
+$selector = "`stripe_custemer_id`='$stripe_custemer_id'";
+$findUser = crud_read('users', 'id' ,$selector)[0]['id'];
+
+if($findUser==''){
+    $data_err['event']="customer.subscription.updated";
+    $data_err['id_event']=$subscription_id;
+    $data_err['comment']="Not find user with reference stripe_custemer_id $stripe_custemer_id new subscription $subscription_id";
+    crud_create('errors_stripe', $data_err);
+}else{
+    $update_data['subscription_period_end']=$currentPeriodEndDate;
+    $update_data['stripe_subscription_id']=$subscription_id;
+    crud_update('users', $update_data, $selector);
+}
+
+push_log('stripe_custemer_id: '.$stripe_custemer_id, basename(__FILE__), 'stipe_log');
+push_log('$subscription_id: '.$subscription_id, basename(__FILE__), 'stipe_log');
+push_log('$currentPeriodEndDate: '.$currentPeriodEndDate, basename(__FILE__), 'stipe_log');
+
 ?>
